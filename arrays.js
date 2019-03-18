55a2dd2bf0ef65ff1035052bfa85a48f7c6c5579
Jennifer Kim <jjkim429@gmail.com>
2019-03-18 21:46:14 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -8,4 +8,9 @@ function destructivelyAddElementToBeginningOfArray(array, element) {
   array.unshift(element)
   return array
 }
-function addElementToEndOfArray()
+function addElementToEndOfArray(array, element) {
+  
+}
+function destructivelyAddElementToEndOfArray(array, element) {
+  
+}
