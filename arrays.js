06cd428cef9befaa0129904afaec57d685bd9ed4
Jennifer Kim <jjkim429@gmail.com>
2019-03-18 21:44:21 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -1,7 +1,8 @@
 var chocolateBars = ['snickers', 'hundred grand', 'kitkat', 'skittles'];
 
 function addElementToBeginningOfArray(array, element) {
- [element, ...array]
+ const newarray = [element, ...array]
+ return newarray
 }
 function destructivelyAddElementToBeginningOfArray(array, element) {
   array.unshift(element)
